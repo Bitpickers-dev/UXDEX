c445ec9b46bc4d56e64c9a7ca58254367f1d250c
adust09 <shoukitsuda@icloud.com>
2022-03-15 20:26:34 +0900

    headerを修正 要らないnodeを削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from '../styles/Home.module.css';
+import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 const Header = () => {
-    return <h2>headerr</h2>;
+    return (
+        < div className={styles.container} >
+            <div className={styles.walletButtons}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+            </div>
+        </div >
+    );
 };
 
 export default Header;
