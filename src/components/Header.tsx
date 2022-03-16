2be09d7e980349f03146375f9405c66a7bef620d
totokazu <tkn.soccerwalker@gmail.com>
2022-03-16 12:11:39 +0900

    ボタンの位置を右に、swap画面を中央にした。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
+const style = {
+    wrapper: `p-4 w-screen flex justify-between items-center`,
+    buttonsContainer: `absolute top-5 right-5 flex w-1/4 justify-end items-center`,
+    button: `items-center bg-[#191B1F] cursor-pointer  `,
+}
+
 const Header = () => {
     return (
-        < div className={styles.container} >
-            <div className={styles.walletButtons}>
-                <WalletMultiButton />
-                <WalletDisconnectButton />
+        <div className={style.wrapper}>
+            <div className={style.buttonsContainer}>
+                <div className={style.button}>
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                </div>
             </div>
         </div >
     );
