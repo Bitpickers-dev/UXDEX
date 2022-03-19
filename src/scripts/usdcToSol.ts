76723b6057e36cf31f493974a951480c0d774b4f
perry <235711131719k@gmail.com>
2022-03-19 12:11:08 +0900

    extract lib function

--- src/scripts/usdcToSol.ts
+++ src/scripts/usdcToSol.ts
@@ -8,20 +8,11 @@ import {
 } from "@solana/web3.js";
 import { PRIVATEKEY } from "./.env";
 import * as bs58 from "bs58";
-import { uxdToSolInstruction } from "../libs/uxdSwap";
-import {
-  findUsdcTokenAddress,
-  findUxdTokenAddress,
-  findWsolTokenAddress,
-} from "../libs/findAssociatedTokenAddress";
-import { usdcToUxdSwapInstruction } from "../libs/swaberSwap";
 import { u64 } from "@saberhq/token-utils";
+import { addUsdcToSolInstruction } from "../libs/composeSwap";
 
 async function main() {
   const mainAccount = Keypair.fromSecretKey(bs58.decode(PRIVATEKEY));
-  const usdcAccount = await findUsdcTokenAddress(mainAccount.publicKey);
-  const uxdAccount = await findUxdTokenAddress(mainAccount.publicKey);
-  const wsolAccount = await findWsolTokenAddress(mainAccount.publicKey);
   const swapUsdcAmount = new u64(2000000);
   const swapUxdAmount = new u64(
     swapUsdcAmount.mul(new BN(995)).div(new BN(1000)).toNumber()
@@ -32,27 +23,14 @@ async function main() {
     feePayer: mainAccount.publicKey,
   });
 
-  // USDC to UXD
-  allocateTransaction.add(
-    usdcToUxdSwapInstruction({
-      account: mainAccount.publicKey,
-      usdcAccount: usdcAccount,
-      uxdAccount: uxdAccount,
-      amountIn: swapUsdcAmount,
-    })
+  const transaction = await addUsdcToSolInstruction(
+    allocateTransaction,
+    mainAccount.publicKey,
+    swapUsdcAmount,
+    swapUxdAmount
   );
 
-  // UXD to SOL
-  allocateTransaction.add(
-    uxdToSolInstruction({
-      signerAccount: mainAccount.publicKey,
-      wsolAccount: wsolAccount,
-      uxdAccount: uxdAccount,
-      amountInMax: swapUxdAmount,
-    })
-  );
-
-  const tx = await sendAndConfirmTransaction(connection, allocateTransaction, [
+  const tx = await sendAndConfirmTransaction(connection, transaction, [
     mainAccount,
     mainAccount,
   ]);
